72b24525615c2ac64119a7aad97f013831ee36dd
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-25 01:09:58 +0900

    connected with chat service

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://edvent.uz", "http://www.edvent.uz", "https://archedu.uz"],
+    origin: ["http://localhost:3000", "https://edvent.uz", "http://www.edvent.uz", "https://archedu.uz"],
     methods: ["GET", "POST"]
   }
 });
